d7f3c8d5d5b46ced500806700f8d71aaf73280ff
Tatikhmel <khmelevskayatati@gmail.com>
2022-10-17 18:48:44 +0300

    feat: remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,10 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  current = l;
+  next = current.next;
+
+  if (!l){
+    return null;
+  } else if(l == null){
+    return 1;
+  }
+
+  while(l.value === k){
+    return removeKFromList(l.next, k);
+  }
+
+  while (next != null) {
+    if (next.value == k) {
+      current.next = next.next;
+
+      if (current.next == null) break;
+  }
+    current = current.next;
+    next = current.next;
+  }
+  return l;
 }
+
 
 module.exports = {
   removeKFromList
